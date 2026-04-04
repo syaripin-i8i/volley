ee92a1a65fa092682edceee3dd26196b03ba2b54
syaripin <66564937+syaripin-i8i@users.noreply.github.com>
2026-04-04 22:07:46 +0900

    fix: remove permission requirement from sidebar to fix menu visibility

--- seat-plugin/src/Config/volley.sidebar.php
+++ seat-plugin/src/Config/volley.sidebar.php
@@ -5,13 +5,11 @@ return [
         'name'          => 'Damage Calc',
         'icon'          => 'fas fa-crosshairs',
         'route_segment' => 'volley',
-        'permission'    => 'volley.view',
         'entries'       => [
             [
-                'name'       => 'Calculator',
-                'icon'       => 'fas fa-calculator',
-                'route'      => 'volley.index',
-                'permission' => 'volley.view',
+                'name'  => 'Calculator',
+                'icon'  => 'fas fa-calculator',
+                'route' => 'volley.index',
             ],
         ],
     ],
